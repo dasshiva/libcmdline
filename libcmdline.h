77c7d9459d5c464efef924ddd5a519a967b7b2ca
Shivashish Das <dasshivashish11@gmail.com>
2025-04-14 13:05:15 +0530

    add docs

--- libcmdline.h
+++ libcmdline.h
@@ -8,48 +8,64 @@ namespace cmdline {
 
 #include <stdint.h>
 
+// Represents the arguments given to a function
 typedef struct {
     union {
-        const char* String;
-        int64_t     Number;
+        const char* String; // String argument to option
+        int64_t     Number; // Number argument to option
     };
     uint32_t Type;
 } OptionArgs;
 
-#define TYPE_STRING (1U << 0)
-#define TYPE_NUMBER (1U << 1)
+// Values for OptionArgs.Type
+#define TYPE_STRING (1U << 0) // OptionArgs.String is the active field
+#define TYPE_NUMBER (1U << 1) // OptionArgs.Number is the active field
 
 typedef int (*OptProcess)(OptionArgs*, uint32_t);
 
-// the option format basically looks like this
+// the format of Option.Fmt basically looks like this
 // opt-format = opt-spec { '-' opt-spec }
 // opt-spec = 's' | 'n'
 // s represents a string and n represents a number
 // Some valid examples include: "s-n-n" or "n-s" but not "ns-n"
+//
+// Represents a single option on the command line
 typedef struct {
-    const char* ShortOption;
-    const char* LongOption;
-    const char* Help;
-    char*       Fmt;
-    OptProcess  Func;
-    OptionArgs* Args;
-    uint32_t    NArgs;
-    uint32_t    Flags;
+    const char* ShortOption; // Shorter form of option (prefixed by '-')
+    const char* LongOption; // Longer form of option (prefixed by "--")
+    const char* Help; // Help text describing what the option does
+    char*       Fmt; // As described above
+    OptProcess  Func; // Function to call after parsing the option
+    OptionArgs* Args; // Pointer to the argumengs given to the option
+    uint32_t    NArgs; // Number of arguments taken by the options
+    uint32_t    Flags; // Flags describing the option3
 } Option;
 
 typedef struct {
-    const char* Name;
-    const char* Version;
-    const char* Copyright;
+    const char* Name; // Name of the program
+    const char* Version; // Version of the program
+    const char* Copyright; // Copyright associated with program
 } Program;
 
-#define OPTION_REQUIRED (1U << 0)
-#define OPTION_DEFAULT  (1U << 1)
+// Values for Option.Flags
+#define OPTION_REQUIRED (1U << 0) // Option must be present
+// Default option. All unused arguments go to this option
+#define OPTION_DEFAULT  (1U << 1) 
+// This option was specified on the command line
+// Do not set this flag yourself if you are a user of the library
 #define OPTION_PRESENT  (1U << 2)
 
+// Register the details of the program
 int ProgramDetails(Program* prog);
-int FreeOptions(Option* options, uint32_t len);
+
+// Free Option.Args
+int FreeOptionArgs(Option* options, uint32_t len);
+
+// Parse command line options from argv having argc command line args
 int ParseOptions(Option** opts, const int argc, const char** argv);
+
+// Generate a help text from all the non-null values of Option.Help from
+// the options present in opts
 void GenerateHelp(const char* progname, Option** opts, const uint32_t oplen);
 
 #define ARG_NULL (-1)
